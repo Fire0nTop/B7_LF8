44dbce55bcb231fb8631b0604d471b277fc1cbb9
Fire0nTop <badhead55@web.de>
2025-03-27 14:25:14 +0100

    added basic components

---
+++ src/app/components/pages/connection-page/connection-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connection-page',
+  imports: [],
+  templateUrl: './connection-page.component.html'
+})
+export class ConnectionPageComponent {
+
+}
