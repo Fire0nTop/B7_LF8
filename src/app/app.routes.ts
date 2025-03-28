59acc24e96f19d759690706ea74762c20ce8e818
admin <maxlinusschulz@gmail.com>
2025-03-28 09:24:44 +0100

    models

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,17 @@
-import { Routes } from '@angular/router';
 import {ConnectionPageComponent} from './components/pages/connection-page/connection-page.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {GameComponent} from './components/game/game.component';
+
 
 export const routes: Routes = [
   { path: '', component: ConnectionPageComponent},
-  { path: '**', redirectTo: '/404'}
+  { path: '**', redirectTo: '/404'},
+  { path: 'game', component: GameComponent },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
