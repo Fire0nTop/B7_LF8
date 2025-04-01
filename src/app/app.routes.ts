2e22b656afbc150b399ef8fb4c0cbf58a4d27c1a
PC <badhead55@web.de>
2025-04-01 16:33:10 +0200

    got php connection working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {StartConnectionComponent} from '@components/connection/start-connection/start-connection.component';
 import {GameComponent} from '@components/game/game.component';
+import {PhpConnectionComponent} from '@components/connection/php-connection/php-connection.component';
 
 
 export const routes: Routes = [
-  { path: '', component: GameComponent },
+  { path: '', component: PhpConnectionComponent },
   { path: 'start-connection', component: StartConnectionComponent },
   { path: '**', redirectTo: '/404'},
 ];
